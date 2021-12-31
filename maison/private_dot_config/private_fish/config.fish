9cb51419f06eb7ce5002344d2fd657afded9a295
Victor Bersy <victor.bersy@hey.com>
2021-12-31 15:04:26 +0100

    Fix fisher automatic install

--- maison/private_dot_config/private_fish/config.fish
+++ maison/private_dot_config/private_fish/config.fish
@@ -1,3 +1,8 @@
+# Install Fisher and my packages
+if status is-interactive && ! functions --query fisher
+    curl --silent --location https://git.io/fisher | source && fisher update
+end
+
 set -g theme_nerd_fonts yes
 set -g fish_prompt_pwd_dir_length 1
 set -g theme_title_display_process yes
