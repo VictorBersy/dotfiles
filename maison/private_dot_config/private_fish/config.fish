881d20e27041b4695b427d31ce29e90619f48e26
Victor Bersy <victor.bersy@hey.com>
2022-01-06 19:37:11 +0100

    Set Europe/Paris as a timezone

--- maison/private_dot_config/private_fish/config.fish
+++ maison/private_dot_config/private_fish/config.fish
@@ -7,5 +7,6 @@ set -g theme_nerd_fonts yes
 set -g fish_prompt_pwd_dir_length 1
 set -g theme_title_display_process yes
 set -gx PROJECT_PATHS ~/Code/Selectra-Dev
+set -gx TZ Europe/Paris
 set -g theme_color_scheme dracula
 set -g theme_display_k8s_context yes
